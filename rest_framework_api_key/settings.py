4a0eb708039180df98493ff13b8daef3c46316f0
florimondmanca <florimond.manca@gmail.com>
2018-10-14 13:13:47 +0200

    Use namespaced settings in package, tests and README.md

--- rest_framework_api_key/settings.py
+++ rest_framework_api_key/settings.py
@@ -2,9 +2,10 @@
 
 from django.conf import settings
 
-API_TOKEN_HEADER = getattr(settings, 'API_TOKEN_HEADER', 'HTTP_API_TOKEN')
-API_SECRET_KEY_HEADER = getattr(settings, 'API_SECRET_KEY_HEADER',
-                                'HTTP_API_SECRET_KEY')
+API_KEY_TOKEN_HEADER = getattr(settings, 'API_KEY_TOKEN_HEADER',
+                               'HTTP_API_TOKEN')
+API_KEY_SECRET_KEY_HEADER = getattr(settings, 'API_KEY_SECRET_KEY_HEADER',
+                                    'HTTP_API_SECRET_KEY')
 
 # The hashing algorithm used to generate the secret key.
 # 'default' means the default configured password hash algorithm is used.
