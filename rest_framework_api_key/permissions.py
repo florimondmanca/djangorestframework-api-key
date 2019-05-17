a449f3c0c60784e13e98902fa77cdf307f0e92b7
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2019-05-17 16:14:02 +0300

    Removed unneccesary codes

--- rest_framework_api_key/permissions.py
+++ rest_framework_api_key/permissions.py
@@ -7,7 +7,6 @@ from .models import APIKey
 
 
 class HasAPIKey(permissions.BasePermission):
-    message = 'Adding customers not allowed.'
     def has_permission(self, request, view) -> bool:
         key = _get_key(request)
 
