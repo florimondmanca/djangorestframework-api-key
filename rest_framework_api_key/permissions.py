3ab2f6b465d645f0d126fd07eba649aab8b974fc
florimondmanca <florimond.manca@gmail.com>
2019-06-15 17:54:55 +0200

    Fix 3.5 compatibility

--- rest_framework_api_key/permissions.py
+++ rest_framework_api_key/permissions.py
@@ -38,13 +38,17 @@ class KeyParser:
 
 
 class BaseHasAPIKey(permissions.BasePermission):
-    model: typing.Type[AbstractAPIKey]
+    model = None
     key_parser = KeyParser()
 
     def get_key(self, request: HttpRequest) -> typing.Optional[str]:
         return self.key_parser.get(request)
 
     def has_permission(self, request: HttpRequest, view: typing.Any) -> bool:
+        assert self.model is not None, (
+            "%s must define `.model` with the API key model to use"
+            % self.__class__.__name__
+        )
         key = self.get_key(request)
         if not key:
             return False
