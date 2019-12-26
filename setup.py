30a12a0069a7943c0f891239d93000e9ae289e68
florimondmanca <florimond.manca@gmail.com>
2019-12-26 14:01:57 +0100

    Add official Python 3.8 support

--- setup.py
+++ setup.py
@@ -44,11 +44,12 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Environment :: Web Environment",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Framework :: Django",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2"
+        "Framework :: Django :: 2.2",
     ],
 )
