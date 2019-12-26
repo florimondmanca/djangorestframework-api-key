c9da587c61648fcc4de4409ffe9df0a39b9cf2c1
florimondmanca <florimond.manca@gmail.com>
2019-12-26 14:06:39 +0100

    Add official Django 3 support

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2"
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
     ],
 )
