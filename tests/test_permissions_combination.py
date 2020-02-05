19ce9240b742d818e18946a31fc5b6539c23daee
Florimond Manca <florimond.manca@gmail.com>
2020-02-05 09:04:59 +0100

    Refactor view_with_permissions as a utility function (#106)

--- tests/test_permissions_combination.py
+++ tests/test_permissions_combination.py
@@ -2,13 +2,14 @@ import pytest
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_api_key.permissions import HasAPIKey
+from .utils import create_view_with_permissions
 
 pytestmark = pytest.mark.django_db
 
 
 @pytest.fixture(name="view")
-def fixture_view(view_with_permissions):
-    return view_with_permissions(HasAPIKey | IsAuthenticated)
+def fixture_view():
+    return create_view_with_permissions(HasAPIKey | IsAuthenticated)
 
 
 def test_if_authenticated_and_no_api_key_then_permission_granted(create_request, view):
