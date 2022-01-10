75b6e895447e8312459a90467dde24f64a91f217
Mariot <mariot.tsitoara@wedge-digital.com>
2022-01-10 23:11:02 +0100

    add argon2 password hashers to heroes app

--- test_project/project/settings.py
+++ test_project/project/settings.py
@@ -69,6 +69,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+# Password hashers
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
+
 
 # Internationalization
 
