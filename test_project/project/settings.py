4c8ffd219d5ce2124b8cb8fb0b87995cbab21775
Mariot <mariot.tsitoara@wedge-digital.com>
2022-01-10 23:13:33 +0100

    run linter

--- test_project/project/settings.py
+++ test_project/project/settings.py
@@ -72,10 +72,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Password hashers
 
 PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.Argon2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
 ]
 
 
