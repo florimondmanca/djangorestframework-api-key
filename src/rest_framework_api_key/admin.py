01bd2904b50d6c56588e7ed9b2ba875a6bcd971f
Mustafa M. A. U. AbuGhazy <mustafa.abu.ghazy@gmail.com>
2022-05-21 17:36:53 +0200

    Enhance APIKey ModelAdmin Class
    - Adding ("expiry_date", "revoked") to list_filter
    - Adding search_help_text (New in Django 4.0)
    - Adding "revoked" to readonly_fields (Depend on the New revoke action to revoke an APIKey)
    - Adding Organized fieldsets
    - Adding ("revoke", "verify") actions
    
    - Adding test-cases cover the changes

--- src/rest_framework_api_key/admin.py
+++ src/rest_framework_api_key/admin.py
@@ -1,8 +1,10 @@
 import typing
 
+from django import forms
 from django.contrib import admin, messages
 from django.db import models
 from django.http.request import HttpRequest
+from django.shortcuts import render
 
 from .models import AbstractAPIKey, APIKey
 
@@ -18,19 +20,26 @@ class APIKeyModelAdmin(admin.ModelAdmin):
         "_has_expired",
         "revoked",
     )
-    list_filter = ("created",)
+    list_filter = ("created", "expiry_date", "revoked")
     search_fields = ("name", "prefix")
+    search_help_text = "Search with: Name, Prefix"  # New in Django 4.0
+    readonly_fields = ("prefix", "revoked", "_has_expired", "created")
+    fieldsets = [
+        ("API-Key", {"fields": [("name", "prefix")]}),
+        ("Status", {"fields": [("revoked", "_has_expired")]}),
+        ("Settings", {"fields": ["expiry_date"]}),
+        ("Record Time Stamps", {"fields": ["created"]}),
+    ]
+    actions = ("revoke", "verify")
 
     def get_readonly_fields(
         self, request: HttpRequest, obj: models.Model = None
     ) -> typing.Tuple[str, ...]:
         obj = typing.cast(AbstractAPIKey, obj)
         fields: typing.Tuple[str, ...]
-
-        fields = ("prefix",)
+        fields = super(APIKeyModelAdmin, self).get_readonly_fields(request, obj) or ()
         if obj is not None and obj.revoked:
-            fields = fields + ("name", "revoked", "expiry_date")
-
+            fields += ("name", "expiry_date")
         return fields
 
     def save_model(
@@ -53,6 +62,59 @@ class APIKeyModelAdmin(admin.ModelAdmin):
             messages.add_message(request, messages.WARNING, message)
         else:
             obj.save()
+
+    def revoke(self, request, queryset):
+        """
+        Implementation of custom action to revoke given API-Key.
+        Requires selecting only one API-Key.
+        """
+        if len(queryset) != 1:
+            return self.message_user(request, "Please select (only) one API-Key to revoke.", messages.ERROR)
+
+        api_key = queryset[0]
+        api_key.revoked = True
+        api_key.save(update_fields=["revoked"])
+        self.log_change(request, api_key, "Revoked the API-Key.")
+
+        return self.message_user(request, "API-Key revoked successfully.", messages.SUCCESS)
+
+    revoke.short_description = "Revoke selected API-Key"
+
+    def verify(self, request, queryset):
+        """
+        Implementation of custom action to verify given API-Key.
+        Requires selecting only one API-Key.
+        """
+
+        class VerificationForm(forms.Form):
+            """
+            Instance of forms.Form to customize its behavior for the :model:"rest_framework_api_key.APIKey" Verify action.
+            """
+            key = forms.CharField(required=True, max_length=200, label="API-Key")
+
+        if len(queryset) != 1:
+            return self.message_user(request, "Please select (only) one API-Key to verify.", messages.ERROR)
+
+        api_key = queryset[0]
+
+        if "apply" in request.POST:
+            form = VerificationForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                key = data.get("key")
+                if api_key.is_valid(key):
+                    self.message_user(request, "Verification Succeed.", messages.SUCCESS)
+                    self.log_change(request, api_key, "Verified the API-Key. Verification Succeed")
+                else:
+                    self.message_user(request, "Verification Failed.", messages.ERROR)
+                    self.log_change(request, api_key, "Verified the API-Key. Verification Failed")
+        else:
+            form = VerificationForm()
+        return render(request, "admin/rest_framework_api_key/apikey/verify.html",
+                      context={**self.admin_site.each_context(request), "opts": self.opts, "instance": api_key,
+                               "form": form, "subtitle": "Verify", "title": api_key.name, "action": "verify"})
+
+    verify.short_description = "Verify Key of selected API-Key"
 
 
 admin.site.register(APIKey, APIKeyModelAdmin)
