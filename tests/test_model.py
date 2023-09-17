f93a1c48c0d26cc5f0c448069827a17de36bdc0a
David Fischer <djfische@gmail.com>
2023-09-17 05:22:09 -0700

    Remove code that updates the PK (#251)
    * Remove code that updates the PK
    
    And note the internal inconsistency.
    
    * After removing the PK upgrade, testing the upgrade is easier

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,6 +2,7 @@ import datetime as dt
 import string
 
 import pytest
+from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 from test_project.heroes.models import Hero, HeroAPIKey
@@ -63,6 +64,29 @@ def test_custom_api_key_model() -> None:
     assert hero_api_key.is_valid(generated_key)
     assert hero_api_key.hero.id == hero.id
     assert hero.api_keys.first() == hero_api_key
+
+
+@pytest.mark.django_db
+def test_api_key_hash_upgrade() -> None:
+    """Tests the hashing algo upgrade from Django's PW hashers to sha512."""
+    key_generator = APIKey.objects.key_generator
+
+    api_key, generated_key = APIKey.objects.create_key(name="test")
+    assert api_key.is_valid(generated_key)
+    assert key_generator.using_preferred_hasher(api_key.hashed_key)
+
+    # Use Django's built-in hashers, the old way of storing a key
+    api_key.hashed_key = make_password(generated_key)
+    api_key.save()
+
+    # Simple sanity check to ensure the hash is still being checked
+    # and that we aren't using the preferred hasher (using Django's slower hashers)
+    assert not api_key.is_valid(key_generator.hash("invalid-key"))
+    assert not key_generator.using_preferred_hasher(api_key.hashed_key)
+
+    # After calling `is_valid`, the key has been upgraded to use the preferred hasher
+    assert api_key.is_valid(generated_key)
+    assert key_generator.using_preferred_hasher(api_key.hashed_key)
 
 
 @pytest.mark.django_db
