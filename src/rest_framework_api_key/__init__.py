02f68e85d2e51f8a2049cb15ee4d5466d38cf1db
Florimond Manca <florimond.manca@protonmail.com>
2025-04-04 10:38:35 +0200

    Version 3.1.0 (#271)

--- src/rest_framework_api_key/__init__.py
+++ src/rest_framework_api_key/__init__.py
@@ -6,6 +6,6 @@ else:
     if django.VERSION < (3, 2):  # pragma: no cover
         default_app_config = "rest_framework_api_key.apps.RestFrameworkApiKeyConfig"
 
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 __all__ = ["__version__", "default_app_config"]
